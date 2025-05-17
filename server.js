2baa2c5bf9d697fa34b4f321835baf95fadee87c
Preetham Aacharya <149096832+Toptha@users.noreply.github.com>
2025-05-17 14:34:53 +0530

    messages

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
+
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const professorRoutes = require('./routes/professor');
@@ -25,4 +28,31 @@ mongoose.connect(process.env.MONGO_URI, {
   .catch((err) => console.error('DB connection error:', err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173', //IMP change to your client URL
+    methods: ['GET', 'POST']
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('join_room', (userId) => {
+    socket.join(userId);
+    console.log(`${userId} joined room`);
+  });
+
+  socket.on('send_message', (data) => {
+    console.log('Message received:', data);
+    io.to(data.receiverId).emit('receive_message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
