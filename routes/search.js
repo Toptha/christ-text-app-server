aa7f617284ae8d95a1b8c83e2b2979f56e46f12b
Preetham Aacharya <149096832+Toptha@users.noreply.github.com>
2025-05-20 10:56:21 +0530

    idek wtf im doing

--- routes/search.js
+++ routes/search.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
 router.get('/', async (req, res) => {
   const searchQuery = req.query.search;
   const currentUserEmail = req.query.currentUser;
